11a7ad4c860632fa8302ed984b9618004caa96d4
fronexxx <igorbotsii42@gmail.com>
2025-08-22 23:45:14 +0300

    Task 9

--- Homeworks/TS_HW_1/hw_from_js_7/Task-9/main.ts
+++ Homeworks/TS_HW_1/hw_from_js_7/Task-9/main.ts
@@ -1,36 +1,45 @@
 // Task-9
-type User = {
-    name: string;
-    age: number;
+
+
+// Array.prototype.myForEach = function (callback) {
+//     for (let i = 0; i < this.length; i++) {
+//         callback(this[i]);
+//     }
+// }
+// users.myForEach(user => console.log(user));
+
+interface Array<T> {
+    myFilter(callback: (value: T) => boolean): T[];
+}
+
+Array.prototype.myFilter = function<T> (callback: (value: T) => boolean): T[] {
+    let result = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i])) {
+            result.push(this[i]);
+        }
+    }
+    return result;
+}
+
+type Created = {
+    name: string,
+    age: number,
     status: boolean;
 };
 
-const usersArrList: User[] = [
-    { name: 'vasya', age: 31, status: false },
-    { name: 'petya', age: 30, status: true },
-    { name: 'kolya', age: 29, status: true },
-    { name: 'olya', age: 28, status: false },
-    { name: 'max', age: 30, status: true },
+// @ts-ignore
+let users: Created[] = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
 ];
-
-function myForEach<T>(arr: T[], callback: (item: T) => void): void {
-    for (let i = 0; i < arr.length; i++) {
-        callback(arr[i]);
-    }
-}
-
-myForEach(usersArrList, (user) => {
-    console.log(user.name);
-});
-
-
-// Array.prototype.myFilter = function (callback: string | number | boolean): usersListArr[] {
-//     let result = [];
-//     for (let i = 0; i < this.length; i++) {
-//         if (callback(this[i])) {
-//             result.push(this[i]);
-//         }
-//     }
-//     return result;
-// }
-// console.log(usersArrList.myFilter(user => user.age > 30));
+console.log(users.myFilter(user => user.age > 30));
